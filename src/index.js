d26703cd1330a787177cb20d0d8c8dfbd982e25f
HardPrecision <mikhreninroman@mail.ru>
2025-03-18 18:06:20 +0500

    2 task

--- src/index.js
+++ src/index.js
@@ -3,17 +3,44 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
-// Отвечает является ли карта уткой.
+class Duck extends Card {
+    constructor() {
+        super('Мирная утка', 2);
+    }
+
+    quacks() {
+        console.log('quack');
+    }
+
+    swims() {
+        console.log('float: both;');
+    }
+}
+
+class Dog extends Card {
+    constructor() {
+        super('Пес-бандит', 3);
+    }
+}
+
+const seriffStartDeck = [
+    new Duck(),
+    new Duck(),
+    new Duck(),
+];
+
+const banditStartDeck = [
+    new Dog(),
+];
+
 function isDuck(card) {
     return card && card.quacks && card.swims;
 }
 
-// Отвечает является ли карта собакой.
 function isDog(card) {
     return card instanceof Dog;
 }
 
-// Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
         return 'Утка-Собака';
@@ -27,40 +54,8 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
-
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
-
-
-// Основа для собаки.
-function Dog() {
-}
-
-
-// Колода Шерифа, нижнего игрока.
-const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-];
-
-// Колода Бандита, верхнего игрока.
-const banditStartDeck = [
-    new Card('Бандит', 3),
-];
-
-
-// Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
-
-// Глобальный объект, позволяющий управлять скоростью всех анимаций.
 SpeedRate.set(1);
-
-// Запуск игры.
 game.play(false, (winner) => {
     alert('Победил ' + winner.name);
 });
